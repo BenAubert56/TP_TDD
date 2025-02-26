454fe9eafab3249dd7cac852f37050b32d0ce1ab
Benjamin Aubert <benjaminaubert22@gmail.com>
2025-02-26 15:45:35 +0100

    first commit

---
+++ src/main/java/fr/formation/tp_tdd/TpTddApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.tp_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/formation/tp_tdd/TpTddApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.tp_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
